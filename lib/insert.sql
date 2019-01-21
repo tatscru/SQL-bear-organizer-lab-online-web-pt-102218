fee941ee8952f2ac46bc38036bd9eb907e0fe484
tatiana.mojica <tatiana.mojica@bustle.com>
2019-01-21 01:50:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
 -- #insert data
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "Female", "Black", "Kind", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "Male", "Brown", "Impulsive", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "Female", "Black", "Diva", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 11, "Male", "Panda", "Talkative", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "Female", "Brown", "Social", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 9, "Male", "Brown", "Conceited", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 10, "Female", "Brown", "Talkative", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, "Male", "Polar", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "F", "Black", "Kind", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "Brown", "Impulsive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Black", "Diva", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 11, "M", "Panda", "Talkative", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "F", "Brown", "Social", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 9, "M", "Brown", "Conceited", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 10, "F", "Brown", "Talkative", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, "M", "Polar", "Angry", 1);
 
 
