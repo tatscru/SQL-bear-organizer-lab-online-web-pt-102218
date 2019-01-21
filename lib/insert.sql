36ab30be44f9daa59bd3f714ca04a85aa56b8115
tatiana.mojica <tatiana.mojica@bustle.com>
2019-01-21 01:50:47 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+-- #insert data
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "F", "Black", "Kind", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "Brown", "Impulsive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Black", "Diva", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 11, "M", "Panda", "Talkative", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "F", "Brown", "Social", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 9, "M", "Brown", "Conceited", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 10, "F", "Brown", "Talkative", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, "M", "Polar", "Angry", 1);
+
+
--- lib/seed.sql
+++ lib/seed.sql
@@ -1,3 +1,4 @@
+-- data for in-memory database
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
 (2,"Rowdy", 10, "M", "black", "intense", 1),
