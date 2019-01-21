a2bcc24f678b63e7e605123fcd1440df0b61440e
tatiana.mojica <tatiana.mojica@bustle.com>
2019-01-21 01:45:02 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 #where you write all of your sql queries 
 
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender= Female" 
+  "SELECT name, age FROM bears WHERE gender= 'Female'" 
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
